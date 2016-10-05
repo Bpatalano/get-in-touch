1d7807b7b1146501048a1692340191f9932a2267
Nick Barry <niko.barry@gmail.com>
2016-10-05 15:11:37 -0700

    chore: Add boilerplate files
    This commit adds a bunch of boilerplate files from https://github.com/mxstbr/react-boilerplate . I've left out some files that I've added to; I'll commit those next. I actually made commits along the way, but for reasons better left unexplained here, I'm starting with a new commit history.
    
    OK, fine, I'll spill. I forked the boilerplate, then started making commits. Then I realized my repo had all the previous commits, and listed all the contributors, of the boilerplate. I know there's a way to use git to create a new repo, ignore all the commits from the boilerplate, and just apply all my commits on top of the boilerplate code, but I'm not taking the time right now to figure out how to do that.

---
+++ app/containers/LanguageProvider/index.js
@@ -0,0 +1,39 @@
+/*
+ *
+ * LanguageProvider
+ *
+ * this component connects the redux state language locale to the
+ * IntlProvider component and i18n messages (loaded from `app/translations`)
+ */
+
+import React from 'react';
+import { connect } from 'react-redux';
+import { IntlProvider } from 'react-intl';
+
+export class LanguageProvider extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <IntlProvider locale={this.props.locale} messages={this.props.messages[this.props.locale]}>
+        {React.Children.only(this.props.children)}
+      </IntlProvider>
+    );
+  }
+}
+
+LanguageProvider.propTypes = {
+  locale: React.PropTypes.string,
+  messages: React.PropTypes.object,
+  children: React.PropTypes.element.isRequired,
+};
+
+const mapStateToProps = languageState => ({
+  locale: languageState.get('locale'),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LanguageProvider);
